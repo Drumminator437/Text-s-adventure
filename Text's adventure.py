cb0d833cfd2ba0a800e64051f1ff0e6c629779eb
Jacob Keating <jacobspiderman2010@gmail.com>
2026-05-07 11:25:26 -0500

    Update Text's adventure.py

--- Text's adventure.py
+++ Text's adventure.py
@@ -1,5 +1,5 @@
-import pygame
-import random
+import pygame 
+import random 
 from pygame.locals import *
 
 screenW = 1000
@@ -12,8 +12,8 @@ blue = (0,0,255)
 
 FPS = 60
 
-
 pygame.init()
+
 level0 = [
     "........................",
     "........................",
@@ -21,7 +21,8 @@ level0 = [
     "........................",
     "........................",
     "XXXXXXXXXXXXXXXXXX..XXXX"
-    ]
+]
+
 level1 = [
     "........................",
     "........................",
@@ -29,46 +30,85 @@ level1 = [
     "..........XXX...........",
     ".....XXX................",
     "XXXXXXXXXXXXXXXXXX..XXXX"
-    ]
+]
+
 level2 = [
     "..................................",
-    ".....XXXXX...XXXXX...XXXX.........",
-    "XX........................XX......",
-    "..XX......................XX......",
-    "......XX.......XX.........XX......",
-    "..........XX.......XX.....XX......",
+    "...X.....X...XXXX....XXXX.........",
+    "...XX.....................XX......",
+    "......X...................XX......",
+    "......XX...X...XX...XX....XX......",
+    ".........................XXX......",
     ".......................XXXXX......",
     "..................XXX.....XX......",
     "............XXX...........XX......",
     "XXXX...XXX................XXXXXXXX"
-    ]
-level3 = [
-    "........................",
-    "........................",
-    "...............XX.......",
-    "..........XXX...........",
-    ".....XXX................",
-    "XXXXXXXXXXXXXXXXXX..XXXX"
-    ]
-level4 = [
-    "........................",
-    "........................",
-    "...............XX.......",
-    "..........XXX...........",
-    ".....XXX................",
-    "XXXXXXXXXXXXXXXXXX..XXXX"
-    ]
-level5 = [
-    "........................",
-    "........................",
-    "...............XX.......",
-    "..........XXX...........",
-    ".....XXX................",
-    "XXXXXXXXXXXXXXXXXX..XXXX"
-    ]
-levels = [level0, level1, level2,level3,level4,level5]
-currentlevel = 0
+]
 
+level3 = [
+    "....................................................................",
+    "....................................................................",
+    "....................................................................",
+    "....................................................................",
+    "X....XX...XX...XX.................................................XX",
+    "XX..................X..............X.....X...X.X.X..X.............XX",
+    "XXX......................XX...XXX......................X..X..X....XX",
+    "XXXX..X..X...X............X.......................XX..............XX",
+    "XXX.............X...XX....X...........XXX........................XXX",
+    "XXX......................XX.........X.............................XX",
+    "XX.........XX...........XXX.....XXXX..............................XX",
+    "XX....XXX......XX......XXXX.....XX................................XX",
+    "XXX.................XXXXXXXXX...XX....XXX...XX....................XX",
+    "XXXX.....................XX.....XXX.............XXX...X...........XX",
+    "XXX...X..X...............XX.....XX..XXX..........X........XXXXXXXXXX",
+    "XXX.........XXX...XX....XXX....XXX.......XX......X..........XXXXXXXX",
+    "....XXX.....XXX........XXXX...................XXXX...XX..X..XXXXXXXX",
+    "......................XXXXX.................X...XX..........XX....XX",
+    ".....................XXXXXX......X..X..X..XXX...XX..................",
+    "XXXX...XXX...X.X..X..XXXXXXXXXXX................XXXX...X..XXXX...XXX"
+]
+
+level4 = [
+    "....................................................................",
+    "....................................................................",
+    "....................................................................",
+    ".............................XXXX...XX..........XX..................",
+    "..............XX.....X....XX.XX.....XX....XXXX........XX............",
+    "..........XX.....XX..........XX.....XXX........X..X......XX.........",
+    "X....XXX.............XX....X.XX.....XX..XX............XX.....XX..XXX",
+    "XX............XX.............XX.....XX.....XX.....................XX",
+    "XXX...............X...XX.....XX.....XX.X.......XX.XX......XX......XX",
+    "XXXX...XXX..............XX...XX.....XX.................XX........XXX",
+    "XXX........X..XX.............XX.....XX......XX......X...........XXX",
+    "XX.................XX........XX.....XX...XX.....XX...............XXX",
+    "XX....XXXX..XX..XX.....XXX...XX.....XXX................XX.......XXXX",
+    "XXX.........................XXX.....XXXX........XX........X..XX..XXX",
+    "XXXX...........XX..........XXXX.....XX...XX...........X...........XX",
+    ".......XXXXX........XX..XX...XX.....XX.......XX..XX.......XX..X..XXX",
+    "................XX...........XX..........XX...........XX........XXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+]
+
+level5 = [
+    "XXXXX...................................................XXXX",
+    "XXXX.....................................................XXX",
+    "XXXX....................................................XXXX",
+    "XXXXX..................................................XXXXX",
+    "XXXX....................................................XXXX",
+    "XXXX....................................................XXXX",
+    "XXXXX....................................................XXX",
+    "XXXXX...................................................XXXX",
+    "XXXX.....................................................XXX",
+    "XXXXX...................................................XXXX",
+    "XXXX.....................................................XXX",
+    "XXXX......XX....................................XX......XXXX",
+    "XXXXXXX..............................................XXXXXXX",
+    "XXXXXXXXX..........................................XXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+]
+
+levels = [level0, level1, level2, level3, level4, level5]
+currentlevel = 5
 
 screen = pygame.display.set_mode((screenW, screenH), pygame.RESIZABLE)
 clock = pygame.time.Clock()
@@ -80,8 +120,23 @@ groundy = 300
 groundimage = pygame.image.load("text.png")
 groundimage = pygame.transform.scale(groundimage, (tilesize, tilesize))
 
+bossrect = pygame.Rect((500,10,100,100))
+bosshp = 100
+
 messegemain = "Text"
 messege = "The adventure of "
+deathtext = "YOU GOT ERASED"
+deathtext2 = "GAME OVER"
+gamestate = "play"
+
+deathtextbox = font.render(deathtext, True, red)
+deathtextbox2 = font.render(deathtext2, True, red)
+
+deathtextrect = deathtextbox.get_rect()
+deathtextrect2 = deathtextbox2.get_rect()
+
+deathtextrect.center = (screenW // 2, screenH // 2 - 20)
+deathtextrect2.center = (screenW // 2, screenH // 2 + 20)
 
 textbox = font.render(messege, True, blue)
 textRect = textbox.get_rect()
@@ -89,25 +144,43 @@ textRect = textbox.get_rect()
 textboxmain = font.render(messegemain, True, blue)
 textRectmain = textboxmain.get_rect()
 
-textRect.center = (500,200) 
+textRect.center = (500,200)
 textRectmain.center = (650,200)
 
-playerx = textRectmain.x 
+playerx = textRectmain.x
 playery = textRectmain.y
+
+bossjumpcooldown = 0
+bossjumppower = -15
+bossspeed = 2
+bossvely = 0
+bossgravity = 0.4
+bossdamagecooldown = 0
+bossphase = 1
+
+playerhp = 100
+playerdamage_cooldown = 0
+knockbackx = 0
+knockbacky = 0
+player_iframes = 0
+boss_iframes = 0
 
 vel_y = 0
 gravity = 0.5
 jumppower = -10
+counter = 0
 
 onground = False
-
 startgravity = False
 
 camerax = 0
 cameray = 0
 scrollmargin = 250
+
+
 def cameracode():
     global camerax, cameray
+
     screenplayerx = playerx - camerax
 
     if screenplayerx > screenW - scrollmargin:
@@ -118,8 +191,8 @@ def cameracode():
 
     if camerax < 0:
         camerax = 0
-    screen_player_y = playery - cameray
 
+    screen_player_y = playery - cameray
     verticalmargin = 150
 
     if screen_player_y > screenH - verticalmargin:
@@ -134,73 +207,267 @@ def cameracode():
     max_camera = level_width() - screenW
     if camerax > max_camera:
         camerax = max_camera
+
+
+def save_code():
+    if currentlevel > 0:
+        levelspawn()
+
+
 def level_width():
     return len(levels[currentlevel][0]) * tilesize
+
+
 def death_code():
-    global running
+    global running, counter
 
     levelheight = len(levels[currentlevel]) * tilesize
 
     if playery > levelheight:
-        running = False
-def player_collision():
-    global playery, vel_y, onground, playerx
+        counter += 1
+        if counter == 3:
+            running = False
+        else:
+            save_code()
+
+
+def collision_y():
+    global playery, vel_y, onground
 
     onground = False
 
-    playerrect = pygame.Rect(
-        playerx,
-        playery,
-        textRectmain.width,
-        textRectmain.height
-    )
+    playerrect = pygame.Rect(playerx, playery, textRectmain.width, textRectmain.height)
 
     for rowIndex, row in enumerate(levels[currentlevel]):
         for columnIndex, tile in enumerate(row):
 
-            if tile == "X":
-                tilerect = pygame.Rect(
-                    columnIndex * tilesize,
-                    rowIndex * tilesize,
-                    tilesize,
-                    tilesize
-                )
+            if tile != "X":
+                continue
 
-                if playerrect.colliderect(tilerect):
+            tilerect = pygame.Rect(
+                columnIndex * tilesize,
+                rowIndex * tilesize,
+                tilesize,
+                tilesize
+            )
 
-                    if vel_y >= 0:
+            if playerrect.colliderect(tilerect):
+
+                if (playerrect.right > tilerect.left + 5 and
+                    playerrect.left < tilerect.right - 5):
+
+                    if vel_y > 0:
                         playery = tilerect.top - textRectmain.height
                         vel_y = 0
                         onground = True
+
+                    elif vel_y < 0:
+                        playery = tilerect.bottom
+                        vel_y = 0
+
+                    playerrect.y = playery
+
+
+def collision_x():
+    global playerx
+
+    playerrect = pygame.Rect(playerx, playery, textRectmain.width, textRectmain.height)
+
+    for rowIndex, row in enumerate(levels[currentlevel]):
+        for columnIndex, tile in enumerate(row):
+
+            if tile != "X":
+                continue
+
+            tilerect = pygame.Rect(
+                columnIndex * tilesize,
+                rowIndex * tilesize,
+                tilesize,
+                tilesize
+            )
+
+            if playerrect.colliderect(tilerect):
+                if playerrect.centerx < tilerect.centerx:
+                    playerx = tilerect.left - textRectmain.width
+                else:
+                    playerx = tilerect.right
+
+                playerrect.x = playerx
+
+
 def draw_level():
     startcollum = int(camerax // tilesize)
     endcollum = startcollum + (screenW // tilesize) + 2
 
     for rowindex, row in enumerate(levels[currentlevel]):
         for collumindex in range(startcollum, min(endcollum, len(row))):
+
             if row[collumindex] == "X":
                 x = collumindex * tilesize - camerax
                 y = rowindex * tilesize - cameray
                 screen.blit(groundimage, (x, y))
+
+
+def levelspawn():
+    global playerx, playery, vel_y, camerax, cameray, boss_spawned
+
+    boss_spawned = False
+
+    for rowIndex in range(len(levels[currentlevel]) - 1, -1, -1):
+        for columnIndex, tile in enumerate(levels[currentlevel][rowIndex]):
+
+            if tile == "X":
+                playerx = columnIndex * tilesize
+                playery = rowIndex * tilesize - textRectmain.height
+                vel_y = 0
+                camerax = 0
+                cameray = 0
+                return
+
+
 def next_level():
-    global currentlevel, playerx, playery, camerax, vel_y
+    global currentlevel, playerx, playery, camerax, vel_y, counter
 
     if playerx > level_width() - 60:
         currentlevel += 1
+        counter = 0
 
         if currentlevel >= len(levels):
             currentlevel = 0
 
-        playerx = 100
-        playery = 100
+        levelspawn()
         vel_y = 0
         camerax = 0
+
+
+def bossspawn():
+    if currentlevel == 5:
+        pygame.draw.rect(
+            screen,
+            blue,
+            (
+                bossrect.x - camerax,
+                bossrect.y - cameray,
+                bossrect.width,
+                bossrect.height
+            )
+        )
+        boss()
+
+def boss_collision():
+    global bossvely
+
+    for rowIndex, row in enumerate(levels[currentlevel]):
+        for columnIndex, tile in enumerate(row):
+
+            if tile != "X":
+                continue
+
+            tilerect = pygame.Rect(
+                columnIndex * tilesize,
+                rowIndex * tilesize,
+                tilesize,
+                tilesize
+            )
+
+            if bossrect.colliderect(tilerect):
+
+                if bossvely > 0:
+                    bossrect.bottom = tilerect.top
+                    bossvely = 0
+
+                elif bossvely < 0:
+                    bossrect.top = tilerect.bottom
+                    bossvely = 0
+def boss():
+    global bosshp, bossvely, bossjumpcooldown
+    global playerhp, vel_y, playerx, playery
+    global bossphase, player_iframes, boss_iframes
+
+    if bosshp <= 50:
+        bossphase = 2
+
+    playerrect = pygame.Rect(playerx, playery, textRectmain.width, textRectmain.height)
+
+    if player_iframes > 0:
+        player_iframes -= 1
+
+    if boss_iframes > 0:
+        boss_iframes -= 1
+
+    if bossphase == 1:
+        speed = bossspeed
+        jump_time = int(FPS * 2.5)
+        boss_jump_power = bossjumppower
+    else:
+        speed = bossspeed * 1.5
+        jump_time = int(FPS * 1.5)
+        boss_jump_power = bossjumppower - 2
+
+    bossjumpcooldown += 1
+    if bossjumpcooldown >= jump_time:
+        bossvely = boss_jump_power
+        bossjumpcooldown = 0
+
+    bossvely += bossgravity
+    bossrect.y += bossvely
+
+    if playerx > bossrect.x:
+        bossrect.x += speed
+    else:
+        bossrect.x -= speed
+
+    boss_collision()
+
+    if playerrect.colliderect(bossrect):
+
+        stomp = (
+            vel_y > 0 and
+            playery + textRectmain.height <= bossrect.top + 18
+        )
+
+        if stomp and boss_iframes == 0:
+            bosshp -= 25
+            bossvely = 6
+            boss_iframes = FPS
+
+            vel_y = jumppower
+
+            print("Player HP:", playerhp, "Boss HP:", bosshp)
+
+        elif not stomp and player_iframes == 0:
+            playerhp -= 10 if bossphase == 1 else 14
+            player_iframes = FPS 
+
+            vel_y = -6
+
+            print("Player HP:", playerhp, "Boss HP:", bosshp)
+def changegamestate():
+    global gamestate
+    if playerhp <= 0:
+        gamestate = "death"  
+def death():
+    screen.fill(black)
+    screen.blit(deathtextbox, deathtextrect)
+    screen.blit(deathtextbox2, deathtextrect2)
 def Gravity():
     global playery, vel_y
 
     vel_y += gravity
     playery += vel_y
+
+
+def jumpcode(keys):
+    global vel_y, onground
+
+    if keys[pygame.K_UP] and onground:
+        vel_y = jumppower
+        onground = False
+
+
+levelspawn()
 running = True
+
 while running:
 
     for event in pygame.event.get():
@@ -219,38 +486,36 @@ while running:
 
     if keys[pygame.K_LEFT]:
         playerx -= 5
+
     if keys[pygame.K_RIGHT]:
         playerx += 5
-    if keys[pygame.K_UP] and onground:
-        vel_y = jumppower
-        onground = False
-    keys = pygame.key.get_pressed()
 
     if playerx < 0:
         playerx = 0
-    
 
+    jumpcode(keys)
     Gravity()
-    player_collision()
+    collision_y()
+    collision_x()
     next_level()
     cameracode()
-
-    starttile = int(camerax // tilesize)
-    endtile = int((camerax + screenW) // tilesize) + 2
-
-    
     draw_level()
-
+    bossspawn()
     textRectmain.x = playerx - camerax
     textRectmain.y = playery - cameray
 
     if currentlevel == 0:
         screen.blit(textbox, textRect)
+
     screen.blit(textboxmain, textRectmain)
 
     pygame.display.flip()
     clock.tick(FPS)
-
-    death_code()
+    changegamestate()
+    if gamestate == "death":
+        death()
+        pygame.display.flip()
+        pygame.time.wait(5000)
+        running = False
 
 pygame.quit()
